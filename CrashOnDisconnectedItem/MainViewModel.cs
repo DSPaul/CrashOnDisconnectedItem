238381a69e310c4c4d258aff020d0ac5a3ed7fe8
DSPAUL <paul.de.smul@gmail.com>
2024-04-20 16:00:25 +0200

    initial commit

---
+++ CrashOnDisconnectedItem/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace CrashOnDisconnectedItem
+{
+    public partial class MainViewModel
+    {
+        public ObservableCollection<string> Destinations { get; } = ["loc1", "loc2", "loc3"];
+
+        [RelayCommand]
+        public void Move(string? destination)
+        {
+            if (string.IsNullOrEmpty(destination))
+            {
+                throw new ArgumentNullException(nameof(destination));
+            }
+
+            //do something here
+            MessageBox.Show($"Moved something to {destination}");
+        }
+    }
+}
---
+++ CrashOnDisconnectedItem/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CrashOnDisconnectedItem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = new MainViewModel();
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Button btn = (Button)sender;
+                btn.ContextMenu!.PlacementTarget = btn;
+                btn.ContextMenu!.IsOpen = !btn.ContextMenu!.IsOpen;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "crash", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
